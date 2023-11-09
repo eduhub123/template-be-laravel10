b4b131228d82533d21a0b49d11f4d099f9ec8e33
Thanh Vu <thanhvd89@gmail.com>
2023-11-09 16:36:08 +0700

    chore: add post, category relationship

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Category extends Model
 {
@@ -27,4 +28,9 @@ class Category extends Model
         self::_CREATED_AT,
         self::_UPDATED_AT,
     ];
+
+    public function posts(): HasMany
+    {
+        return $this->hasMany(Post::class);
+    }
 }
--- app/Models/Post.php
+++ app/Models/Post.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Post extends Model
 {
@@ -33,4 +34,9 @@ class Post extends Model
         self::_CREATED_AT,
         self::_UPDATED_AT,
     ];
+
+    public function category(): BelongsTo
+    {
+        return $this->belongsTo(Category::class);
+    }
 }
